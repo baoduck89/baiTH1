663bf45fa6ba5667d30b9516d7ea75634af3adea
Đức Bảo <ndbpro99@gmail.com>
2020-10-10 19:17:40 +0700

    test

---
+++ bai1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace baithuchanh1
+{
+
+    class Diem
+    {
+        private double x;
+        private double y;
+        public Diem()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Diem(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void nhap()
+        {
+            Console.Write("Nhap Hoanh do : ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Nhap Tung do : ");
+            y = double.Parse(Console.ReadLine());
+        }
+        public void hienThi()
+        {
+            Console.WriteLine("Toa do la : ({0},{1})", x, y);
+        }
+        public double Khoangcach(Diem d1, Diem d2)
+        {
+            double kc = Math.Sqrt((d1.x - d2.x) * (d1.x - d2.x) + (d1.y - d2.y) * (d1.y - d2.y));
+            return kc;
+        }
+        static void Main1(string[] args)
+        {
+            Diem t = new Diem();
+            t.nhap();
+            t.hienThi();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bai5.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+namespace B5_TH1_OOP
+{
+    class MaTran
+    {
+        private int sh, sc;
+        private int[,] a;
+        public MaTran()
+        {
+            sh = sc = 2;
+            a = new int[sh, sc];
+        }
+        public MaTran(int sh, int sc)
+        {
+            this.sh = sh;
+            this.sc = sc;
+            a = new int[sh, sc];
+        }
+        public MaTran(MaTran t2)
+        {
+            this.sh = t2.sh;
+            this.sc = t2.sc;
+            this.a = new int[sh, sc];
+            for (int i = 0; i < sh; i++)
+                for (int j = 0; j < sc; j++)
+                    this.a[i, j] = t2.a[i, j];
+        }
+        public void Print()
+        {
+            Console.WriteLine("Cac phan tu cua ma tran la");
+            for (int i = 0; i < sh; i++)
+            {
+                for (int j = 0; j < sc; j++)
+                    Console.Write("{0}\t", a[i, j]);
+                Console.WriteLine();
+            }
+        }
+        public void Nhap()
+        {
+            Console.Write("Nhap so hang: "); sh = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so cot: "); sc = int.Parse(Console.ReadLine());
+            a = new int[sh, sc];
+            Console.WriteLine("Nhap thong tin cho cac phan tu cua ma tran");
+            for (int i = 0; i < sh; i++)
+                for (int j = 0; j < sc; j++)
+                {
+                    Console.Write("a[{0},{1}] = ", i, j);
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+        }
+        public MaTranTong(MaTran t2)
+        {
+            if (this.sh == t2.sh && this.sc == t2.sc)
+            {
+                MaTran t = new MaTran(this.sh, this.sc);
+                for (int i = 0; i < sh; i++)
+                    for (int j = 0; j < sc; j++)
+                        t.a[i, j] = this.a[i, j] + t2.a[i, j];
+                return t;
+            }
+            else return null;
+        }
+        public MaTranHieu(MaTran t2)
+        {
+            if (this.sh == t2.sh && this.sc == t2.sc)
+            {
+                MaTran t = new MaTran(this.sh, this.sc);
+                for (int i = 0; i < sh; i++)
+                    for (int j = 0; j < sc; j++)
+                        t.a[i, j] = this.a[i, j] - t2.a[i, j];
+                return t;
+            }
+            else return null;
+        }
+        public bool TuongThich()
+        {
+            if (this.sh == t2.sh && this.sc == t2.sc)
+                return true;
+            else return false;
+        }
+        static void Main5(string[] args)
+        {
+        }
+    }
+}
